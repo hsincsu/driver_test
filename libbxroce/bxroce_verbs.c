c9367adca46751ba87a73b9075991fcef12c03b8
hs <hs@example.com>
2020-06-01 19:48:34 +0800

    fix debug

--- libbxroce/bxroce_verbs.c
+++ libbxroce/bxroce_verbs.c
@@ -164,7 +164,8 @@ struct ibv_mr *bxroce_reg_mr(struct ibv_pd *pd, void *addr, size_t length,uint64
 	BXPRMR("------------check reg mr 's sg info --------------\n");
 	BXPRMR("stride:0x%x \n", stride);
 	BXPRMR("num_sge:0x%x \n", resp.sg_phy_num);
-	BXPRMR("resp[0]'s size is: 0x%x \n",resp.sg_phy_addr[0]);
+	BXPRMR("resp[0]'s addr is: 0x%x \n",resp.sg_phy_addr[0]);
+	BXPRMR("resp[1]'s addr is : 0x%x \n",resp.sg_phy_addr[1]);
 	BXPRMR("resp[0]'s size is: 0x%x  \n",resp.sg_phy_size[0]);
 	BXPRMR("mr's va: 0x%x \n",hca_va);
 	for(i = 0 ; i< num_sg; i++)
