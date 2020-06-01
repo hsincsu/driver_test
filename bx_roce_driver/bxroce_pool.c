d7f21340d6e79da7b4a7ae0a431142fbdc17846a
hs <hs@example.com>
2020-06-01 21:46:08 +0800

    fix debug

--- bx_roce_driver/bxroce_pool.c
+++ bx_roce_driver/bxroce_pool.c
@@ -601,7 +601,7 @@ int bxroce_mem_init_user(struct bxroce_pd *pd, u64 start, u64 length, u64 iova, 
 		uresp.sg_phy_num = num_buf;
 		uresp.offset	 = mr->offset;
 		BXROCE_PR("bxroce:sg_phy_num:0x%x , offset: 0x%x \n",uresp.sg_phy_num, uresp.offset);
-		BXROCE_PR("bxroce:uresp's size is :0x%x , udata's size\n",sizeof(uresp),udata->outlen);
+		BXROCE_PR("bxroce:uresp's size is :0x%x , udata's size: 0x%x\n",sizeof(uresp),udata->outlen);
 
 		if(udata){
 			BXROCE_PR("get in udata \n");
