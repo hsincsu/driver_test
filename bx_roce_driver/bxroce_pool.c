73e9e6ba653c4a918b3ebb79f1e9835db061a0ed
hs <hs@example.com>
2020-06-01 15:08:14 +0800

    fix debug

--- bx_roce_driver/bxroce_pool.c
+++ bx_roce_driver/bxroce_pool.c
@@ -600,6 +600,7 @@ int bxroce_mem_init_user(struct bxroce_pd *pd, u64 start, u64 length, u64 iova, 
 		
 		uresp.sg_phy_num = num_buf;
 		uresp.offset	 = mr->offset;
+		BXROCE_PR("bxroce:sg%d, sg_phy_num:0x%x , offset: 0x%x \n",uresp.sg_phy_num, uresp.offset);
 
 		if(udata){
 		status = ib_copy_to_udata(udata, &uresp, sizeof(uresp));
