defdde05a653603c61d143360bb7e8ab684854a4
hsincsu <alittlefatboy@foxmail.com>
2020-06-11 16:54:33 +0800

    fix debu

--- libbxroce/bxroce_hw.c
+++ libbxroce/bxroce_hw.c
@@ -21,7 +21,7 @@ uint32_t bxroce_mpb_reg_read(void *iova, uint32_t module_addr, uint32_t regaddr)
 	*(__le32 *)((uint8_t *)(iova) + MPB_WRITE_ADDR) = htole32(module_addr + regaddr);
 
 	udma_to_device_barrier();
-    regval = le32toh(*(__le32 *)((uint8_t *)(iova) + MPB_RW_DATA);
+    regval = le32toh(*(__le32 *)((uint8_t *)(iova) + MPB_RW_DATA));
 
     return regval;
 }
