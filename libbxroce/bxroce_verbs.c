167bb2e33319683636fa5496e18b1d76760888b0
hsincsu <alittlefatboy@foxmail.com>
2020-06-11 14:29:24 +0800

    fix debug

--- libbxroce/bxroce_verbs.c
+++ libbxroce/bxroce_verbs.c
@@ -438,20 +438,12 @@ struct ibv_qp *bxroce_create_qp(struct ibv_pd *pd,
 	tmpvalue = le32toh(*(__le32 *)((uint8_t *)(qp->iova) + MPB_RW_DATA));
 	printf("TLBINIT 0x%x \n",tmpvalue);
 
-	*(__le32 *)((uint8_t *)(qp->iova) + MPB_WRITE_ADDR) = htole32(PGU_BASE + SOCKETID);
-	tmpvalue = le32toh(*(__le32 *)((uint8_t *)(qp->iova) + MPB_RW_DATA));
-	printf("socket id 0x%x \n",tmpvalue);
-
-	*(__le32 *)((uint8_t *)(qp->iova) + MPB_WRITE_ADDR) = htole32(PGU_BASE + TLBINIT);
-	tmpvalue = le32toh(*(__le32 *)((uint8_t *)(qp->iova) + MPB_RW_DATA));
-	printf("TLBINIT 0x%x \n",tmpvalue);
-
-	qp->iova + MPB_WRITE_ADDR = PGU_BASE + SOCKETID;
-	tmpvalue = qp->iova + MPB_RW_DATA;
+	*(qp->iova + MPB_WRITE_ADDR) = PGU_BASE + SOCKETID;
+	tmpvalue = *(qp->iova + MPB_RW_DATA);
 	printf("other way: socketid :0x%x \n");
 
-	qp->iova + MPB_WRITE_ADDR = PGU_BASE + TLBINIT;
-	tmpvalue = qp->iova + MPB_RW_DATA;
+	*(qp->iova + MPB_WRITE_ADDR) = PGU_BASE + TLBINIT;
+	tmpvalue = *(qp->iova + MPB_RW_DATA);
 	printf("other way: tlbinit: 0x%x \n");
 
 	printf("test uer hw write & read end\n");
@@ -1235,12 +1227,12 @@ static void bxroce_ring_sq_hw(struct bxroce_qp *qp) {
 	tmpvalue = le32toh(*(__le32 *)((uint8_t *)(qp->iova) + MPB_RW_DATA));
 	printf("TLBINIT 0x%x \n",tmpvalue);
 
-	qp->iova + MPB_WRITE_ADDR = PGU_BASE + SOCKETID;
-	tmpvalue = qp->iova + MPB_RW_DATA;
+	*(qp->iova + MPB_WRITE_ADDR) = PGU_BASE + SOCKETID;
+	tmpvalue = *(qp->iova + MPB_RW_DATA);
 	printf("other way: socketid :0x%x \n");
 
-	qp->iova + MPB_WRITE_ADDR = PGU_BASE + TLBINIT;
-	tmpvalue = qp->iova + MPB_RW_DATA;
+	*(qp->iova + MPB_WRITE_ADDR) = PGU_BASE + TLBINIT;
+	tmpvalue = *(qp->iova + MPB_RW_DATA);
 	printf("other way: tlbinit: 0x%x \n");
 
 	printf("test uer hw write & read end\n");
