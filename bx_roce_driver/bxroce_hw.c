0e40ae6e8d1db390dfc7823aa5988aa62cc650f1
hsincsu <alittlefatboy@foxmail.com>
2020-07-24 16:52:12 +0800

    test

--- bx_roce_driver/bxroce_hw.c
+++ bx_roce_driver/bxroce_hw.c
@@ -610,7 +610,7 @@ static int bxroce_init_pgu_cq(struct bxroce_dev *dev)
 	bxroce_mpb_reg_write(dev,base_addr,PGU_BASE,INTRMASK,0x4000);//open all mask
 	bxroce_mpb_reg_write(dev,base_addr,PGU_BASE,UPLINKDOWNLINK,0x00800400);
 	bxroce_mpb_reg_write(dev,base_addr,PGU_BASE,GENRSP,0x0100100);
-	bxroce_mpb_reg_write(dev,base_addr,PGU_BASE,CFGRNR,0x04010041);
+	bxroce_mpb_reg_write(dev,base_addr,PGU_BASE,CFGRNR,0x04010043);
 	
 	BXROCE_PR("bxroce: bxroce_init_pgu_cq end \n");//added by hs
 	return err;
