73ea06394a853b588eabd302bd254ff2260f9b89
hsincsu <alittlefatboy@foxmail.com>
2020-08-05 14:56:09 +0800

    test

--- libbxroce/bxroce_hw.c
+++ libbxroce/bxroce_hw.c
@@ -17,7 +17,7 @@ uint32_t bxroce_mpb_reg_read(void *iova, uint32_t module_addr, uint32_t regaddr)
    
     uint32_t regval = 0;
 
-    
+    udma_to_device_barrier();
 	*(__le32 *)((uint8_t *)(iova) + MPB_WRITE_ADDR) = htole32(module_addr + regaddr);
 
 	udma_from_device_barrier();
