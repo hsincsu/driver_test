7e5aa5de88abf8498a64be24ff13a88c2c5b83e9
hsincsu <alittlefatboy@foxmail.com>
2020-08-05 14:51:01 +0800

    test

--- libbxroce/bxroce_hw.c
+++ libbxroce/bxroce_hw.c
@@ -4,12 +4,12 @@ void bxroce_mpb_reg_write(void *iova, uint32_t module_addr, uint32_t regaddr, ui
 {
    
 
-    udma_to_device_barrier();
+    
 	*(__le32 *)((uint8_t *)(iova) + MPB_WRITE_ADDR) = htole32(module_addr + regaddr);
 
-	
-    *(__le32 *)((uint8_t *)(iova) + MPB_RW_DATA) = htole32(value);
 	udma_to_device_barrier();
+    *(__le32 *)((uint8_t *)(iova) + MPB_RW_DATA) = htole32(value);
+	
 }
 
 uint32_t bxroce_mpb_reg_read(void *iova, uint32_t module_addr, uint32_t regaddr)
@@ -17,11 +17,11 @@ uint32_t bxroce_mpb_reg_read(void *iova, uint32_t module_addr, uint32_t regaddr)
    
     uint32_t regval = 0;
 
-    udma_to_device_barrier();
+    
 	*(__le32 *)((uint8_t *)(iova) + MPB_WRITE_ADDR) = htole32(module_addr + regaddr);
 
-	
+	udma_from_device_barrier();
     regval = le32toh(*(__le32 *)((uint8_t *)(iova) + MPB_RW_DATA));
-    udma_from_device_barrier();
+    
     return regval;
 }
