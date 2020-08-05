a3151172dfff4ad64bd23ea84832d7e88b1bee8e
hsincsu <alittlefatboy@foxmail.com>
2020-08-05 17:32:22 +0800

    test

--- libbxroce/bxroce_main.h
+++ libbxroce/bxroce_main.h
@@ -411,13 +411,13 @@ uint32_t bxroce_mpb_reg_read(void *iova, uint32_t module_addr, uint32_t regaddr)
 
 //#define BXROCE_MRINFO
 //#define BXROCE_QPINFO
-//#define BXROCE_CQINFO
+#define BXROCE_CQINFO
 //#define BXROCE_PDINFO
 
 #define BXROCE_OTHINFO
 //#define BXROCE_HWINFO
 //#define BXROCE_RECVINFO
-//#define BXROCE_SENDINFO
+#define BXROCE_SENDINFO
 
 
 #ifdef BXROCE_RECVINFO
