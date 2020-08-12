48bf8189531a636c37b16a06debda5197bfd471a
hs <hs@example.com>
2020-08-12 16:29:24 +0800

    test

--- bx_mac_hw.c
+++ bx_mac_hw.c
@@ -3291,7 +3291,7 @@ static int mac_hw_init(struct mac_pdata *pdata)
 		regval = 0x08040201;
 		writel(regval, pdata->mac_regs + 0x160);
 
-		regval = 0x00c02010;
+		regval = 0x00602010;
 		writel(regval, pdata->mac_regs + 0x164);
         #endif
     #if 0
@@ -3319,7 +3319,7 @@ static int mac_hw_init(struct mac_pdata *pdata)
         writel(regval,pdata->mac_regs + 0x1034);
 #endif
         #if 1
-        regval = 0x00c02000;//0x00002000; //to channel 6;
+        regval = 0x00602000;//0x00002000; //to channel 6;
 	    writel(regval, pdata->mac_regs + 0x1044); // config mtl_tc_prty_map1
         #endif
         #if 0
